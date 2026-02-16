d7a66fa4950a12e24f751dc4bdb28b28fb7771c6
unk286 <177930405+unk286@users.noreply.github.com>
2026-02-16 08:25:07 +0000

    food menu update

--- themenu.py
+++ themenu.py
@@ -2,23 +2,34 @@ import tkinter as tk
 
 root = tk.Tk()
 
-foods = "burger pizza chips burrito"
+foods = "burger pizza fries burrito"
 foods = foods.split()
+images = {
+    "burger": tk.PhotoImage(file="burger.png"),
+    "pizza": tk.PhotoImage(file="pizza.png"),
+    "fries": tk.PhotoImage(file="fries.png"),
+    "burrito": tk.PhotoImage(file="burrito.png")
+}
+
 
 root.geometry("300x300+1500+400")
 root.title("fast food place")
 
 def orderfood():
     selected = listbox.curselection()
+    if not selected:
+        return None
     food = listbox.get(selected)
     print(food)
     orderup = tk.Toplevel()
     orderup.title("Your meal")
-    tk.Label(orderup, text=f"Heres your {food}").pack()
+    img = images[food]
+    tk.Label(orderup, image=img).pack()
+    tk.Label(orderup, text=f"Heres your {food}", font=("font",20)).pack(pady=10)
         
 
 
-tk.Label(root, text="Big menu:", font=("font", 20)).pack(pady=10)
+tk.Label(root, text="Pick a food:", font=("font", 20)).pack(pady=10)
 listbox = tk.Listbox(root, height=10, width=20)
 for food in foods:
     listbox.insert(tk.END, food)
